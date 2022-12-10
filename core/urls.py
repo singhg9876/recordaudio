9e867b50abcd0aafd42acaf10e24eb2ca850cdcd
singhg9876 <60215747+singhg9876@users.noreply.github.com>
2022-12-10 17:06:05 +0530

    Add files via upload

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = "core"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("record/", views.record, name="record"),
+    path("record/detail/<uuid:id>/", views.record_detail, name="record_detail"),
+    path("download_excel_9425006756", views.downloadExcel, name="downloadExcel")
+]
